36071a857f14c79fc3d4288ad95c4d3fa2cfd3f2
sebastian <77418085+esby-space@users.noreply.github.com>
2021-12-03 15:37:59 -0500

    servo?

---
+++ plusServo.java
@@ -0,0 +1,106 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@TeleOp(name="TestBot: TeleOP", group="TestBot")
+public class plusServo extends LinearOpMode {
+
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+    // private DcMotor linearSlide;
+    // private DcMotor verticalSweeper;
+    // private DcMotor duckTurner;
+     private Servo deliverySystem;
+
+    @Override
+    public void runOpMode() {
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+        frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeft");
+        backRightMotor = hardwareMap.dcMotor.get("backRight");
+        deliverySystem = hardwareMap.servo.get("deliverySystem");
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addData("Say", "Hello World!");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+
+            telemetry.addData("Say", gamepad1.dpad_down);
+
+            telemetry.update();
+
+            boolean deliverySystemCCW = gamepad1.left_bumper;
+            boolean deliverSystemCW = gamepad1.right_bumper;
+
+            if (deliverSystemCW) {
+                deliverySystem.setPosition(0.2);
+            }
+
+            if (deliverySystemCCW) {
+                deliverySystem.setPosition(-0.2);
+            }
+
+            // Output the safe vales to the motor drives.
+            //robot.leftDrive.setPower(drive); // originally left
+//            robot.rightDrive.setPower(right);
+
+            // Use gamepad left & right Bumpers to open and close the claw
+//            if (gamepad1.right_bumper)
+//                clawOffset += CLAW_SPEED;
+//            else if (gamepad1.left_bumper)
+//                clawOffset -= CLAW_SPEED;
+
+            // Move both servos to new position.  Assume servos are mirror image of each other.
+//            clawOffset = Range.clip(clawOffset, -0.5, 0.5);
+//            robot.leftClaw.setPosition(robot.MID_SERVO + clawOffset);
+//            robot.rightClaw.setPosition(robot.MID_SERVO - clawOffset);
+
+            // Use gamepad buttons to move arm up (Y) and down (A)
+//            if (gamepad1.y)
+//                robot.leftArm.setPower(robot.ARM_UP_POWER);
+//            else if (gamepad1.a)
+//                robot.leftArm.setPower(robot.ARM_DOWN_POWER);
+//            else
+//                robot.leftArm.setPower(0.0);
+
+            // Send telemetry message to signify robot running;
+//            telemetry.addData("claw",  "Offset = %.2f", clawOffset);
+//            telemetry.addData("left",  "%.2f", left);
+//            telemetry.addData("right", "%.2f", right);
+//               telemetry.addData("yay!");
+            //telemetry.update();
+
+            // Pace this loop so jaw action is reasonable speed.
+            idle();
+            // }
+        }
+    }
+}
