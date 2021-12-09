5d0a9cd96693abbdd4dd62f6664f927bc73b1f45
sebastian <77418085+esby-space@users.noreply.github.com>
2021-12-08 20:19:38 -0500

    refactor

--- README.md
+++ README.md
@@ -1,18 +1,46 @@
-# FTC 2021
-**Team ID: 14413**
+# Trinity Robotics FTC 2021
+### Team ID: 14413
 
-## how to edit
+## how to run and edit
 
-**download the FTC source code for freight frenzy and copy these files into teamCode.**
-- note that they will not run without a control hub hooked up to your Windows-emulated device. 
+1. download the ftc sdk for freight frenzy
+2. copy and paste these files in the TeamCode folder
+3. plug your computer into the control hub, select REV control hub from device manage, and press the play button next to it
+4. wait for gradle to build the project
+5. wait for a "launch successful" at the bottom
+6. unplug from the robot, connect the driver hub to control hub via wifi
+7. robot go beep boop
 
 ## files
 
-1. TestOP - robot code that works
-- makes the robot move forward, backwards, strafe, and rotate
-2. testing - newer additions that haven't been tested
-- delivery system, linear slide, duck wheel, etc.
+1. **TestOP** - basic robot code
+- moves forward, back, strafe, and rotate
+2. **TeleOP** - newer additions
 
+## mappings
+
+gamepad | function
+------- | --------
+left joystick up | move forward
+left joystick down | move down
+left joystick left | strafe left
+left joystick right | strafe right
+right joystick left | rotate left
+right joystick right | rotate right
+y button | linear slide level 1
+x button | linear slide level 2
+a button | linear slide level 3
+b button | linear slide down
+left bumper | delivery system ccw
+right bumper | delivery system cw
+d-pad up* | intake system ccw
+d-pad down* | intake system cw
+left trigger | duck wheel ccw
+right trigger | duck wheel cw
+
+ \*currently not implemented 
+
+---
 ## i would love your help!
 
 check the team discord for an ongoing list of programming projects that still need completion.
@@ -21,6 +49,3 @@ even without the control hub there's plenty of research and coding to be done!
 ## the software team!
 - captain: lake
   - email: patience.gifford23@trinityschoolnyc.org
-- enli
-- jasmine
-- sebastian
---
+++ TeleOP.java
@@ -0,0 +1,124 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Testing TeleOP", group = "Test Bot")
+public class TeleOP extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        // map the motors to the robot
+        // control hub:
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // expansion hub motors
+        DcMotor linearSlide = hardwareMap.dcMotor.get("linearSlide");
+        DcMotor intakeSystem = hardwareMap.dcMotor.get("intakeSystem");
+        DcMotor duckWheel = hardwareMap.dcMotor.get("duckSystem");
+
+        // expansion hub servos
+        Servo deliverySystem = hardwareMap.servo.get("deliverySystem");
+
+        // wait for start signal to be given
+        waitForStart();
+        telemetry.addData("Start", "Hello, World!");
+        telemetry.update();
+
+        // initialize the timer
+        ElapsedTime motorTimer = new ElapsedTime();
+        double startTimer = 0;
+
+        // when the start button is pushed, repeat this until end
+        while (opModeIsActive()) {
+
+            // Movement and rotation of the robot with mecanum wheels
+            double move_y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double move_x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double move_rotate = -gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(move_y) + Math.abs(move_x) + Math.abs(move_rotate), 1);
+            double frontLeftPower = (move_y + move_x + move_rotate) / denominator;
+            double backLeftPower = (move_y - move_x + move_rotate) / denominator;
+            double frontRightPower = (move_y - move_x - move_rotate) / denominator;
+            double backRightPower = (move_y + move_x - move_rotate) / denominator;
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
+
+            // delivery system
+            if (gamepad1.left_bumper) {
+                deliverySystem.setPosition(0.5 + 0.2);
+                telemetry.addData("Delivery System", "Counter-clockwise");
+                telemetry.update();
+            } else if (gamepad1.right_bumper) {
+                deliverySystem.setPosition(0.5 - 0.2);
+                telemetry.addData("Delivery System", "Clockwise");
+                telemetry.update();
+            }
+
+            // duck wheel
+            double duckWheelCCW = gamepad1.left_trigger;
+            double duckWheelCW = gamepad1.right_trigger;
+
+            if (duckWheelCCW > 0) {
+                duckWheel.setPower(-duckWheelCCW);
+                telemetry.addData("Duck Wheel", "Counter-clockwise");
+                telemetry.update();
+            } else if (duckWheelCW > 0) {
+                duckWheel.setPower(duckWheelCW);
+                telemetry.addData("Duck Wheel", "Clockwise");
+                telemetry.update();
+            } else {
+                duckWheel.setPower(0);
+            }
+
+            // linear slide
+            boolean y_button = gamepad1.y;
+            boolean x_button = gamepad1.x;
+            boolean a_button = gamepad1.a;
+            boolean b_button = gamepad1.b;
+            double seconds = 0;
+            double rate = 2; // inches for 1 rotation
+
+            // determine the number of seconds from the bottom the linear slide has to run for
+            // decimal-ly numbers based on heights of the wedding cake thingy
+            if (y_button) {
+                seconds = 0.4 / rate;
+                startTimer = motorTimer.time();
+                telemetry.addData("Linear Slide", "Going to level 1");
+            } else if (x_button) {
+                seconds = 1.2 / rate;
+                startTimer = motorTimer.time();
+                telemetry.addData("Linear Slide", "Going to level 2");
+            } else if (a_button) {
+                seconds = 2.0 / rate;
+                startTimer = motorTimer.time();
+                telemetry.addData("Linear Slide", "Going to level 3");
+            } else if (b_button) {
+                linearSlide.setPower(-1);
+                telemetry.addData("Linear Slide", "Going down");
+            }
+            telemetry.update();
+
+            while (motorTimer.time() < startTimer + seconds) {
+                linearSlide.setPower(1);
+            }
+            linearSlide.setPower(0);
+
+            idle();
+        }
+    }
+}
