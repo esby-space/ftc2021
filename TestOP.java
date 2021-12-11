22b166981d6bec8029e2d8900a69c3dc675947ef
lakegifford23 <56414210+lakegifford23@users.noreply.github.com>
2021-12-11 15:23:25 -0500

    Update TestOP.java

--- TestOP.java
+++ TestOP.java
@@ -1,130 +1,124 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
-
-
-@TeleOp(name="TestBot: TeleOP", group="TestBot")
+@TeleOp(name = "Testing TeleOP", group = "Test Bot")
 public class TestOP extends LinearOpMode {
-
-    private DcMotor frontLeftMotor;
-    private DcMotor frontRightMotor;
-    private DcMotor backLeftMotor;
-    private DcMotor backRightMotor;
- //   private DcMotor linearSlide;
-  //  private DcMotor verticalSweeper;
-   // private DcMotor duckTurner;
-   // private Servo deliverySystem;
 
     @Override
     public void runOpMode() {
-        frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
-        frontRightMotor = hardwareMap.dcMotor.get("frontRight");
-        backLeftMotor = hardwareMap.dcMotor.get("backLeft");
-        backRightMotor = hardwareMap.dcMotor.get("backRight");
-        //
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-       backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        // map the motors to the robot
+        // control hub:
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
 
-        telemetry.addData("Say", "Hello World!");    //
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // expansion hub motors
+        DcMotor linearSlide = hardwareMap.dcMotor.get("linearSlide");
+        DcMotor intakeSystem = hardwareMap.dcMotor.get("intakeSystem");
+        DcMotor duckWheel = hardwareMap.dcMotor.get("duckSystem");
+
+        // expansion hub servos
+        Servo deliverySystem = hardwareMap.servo.get("deliverySystem");
+
+        // wait for start signal to be given
+        waitForStart();
+        telemetry.addData("Start", "Hello, World!");
         telemetry.update();
 
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
+        // initialize the timer
+        ElapsedTime motorTimer = new ElapsedTime();
+        double startTimer = 0;
 
-        // run until the end of the match (driver presses STOP)
+        // when the start button is pushed, repeat this until end
         while (opModeIsActive()) {
 
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
+            // Movement and rotation of the robot with mecanum wheels
+            double move_y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double move_x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double move_rotate = -gamepad1.right_stick_x;
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            double denominator = Math.max(Math.abs(move_y) + Math.abs(move_x) + Math.abs(move_rotate), 1);
+            double frontLeftPower = (move_y + move_x + move_rotate) / denominator;
+            double backLeftPower = (move_y - move_x + move_rotate) / denominator;
+            double frontRightPower = (move_y - move_x - move_rotate) / denominator;
+            double backRightPower = (move_y + move_x - move_rotate) / denominator;
 
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
 
-            telemetry.addData("Say", gamepad1.dpad_down);
+            // delivery system
+            if (gamepad1.left_bumper) {
+                deliverySystem.setPosition(0.5 + 0.2);
+                telemetry.addData("Delivery System", "Counter-clockwise");
+                telemetry.update();
+            } else if (gamepad1.right_bumper) {
+                deliverySystem.setPosition(0.5 - 0.2);
+                telemetry.addData("Delivery System", "Clockwise");
+                telemetry.update();
+            }
 
+            // duck wheel
+            double duckWheelCCW = gamepad1.left_trigger;
+            double duckWheelCW = gamepad1.right_trigger;
+
+            if (duckWheelCCW > 0) {
+                duckWheel.setPower(-duckWheelCCW);
+                telemetry.addData("Duck Wheel", "Counter-clockwise");
+                telemetry.update();
+            } else if (duckWheelCW > 0) {
+                duckWheel.setPower(duckWheelCW);
+                telemetry.addData("Duck Wheel", "Clockwise");
+                telemetry.update();
+            } else {
+                duckWheel.setPower(0);
+            }
+
+            // linear slide
+            boolean y_button = gamepad1.y;
+            boolean x_button = gamepad1.x;
+            boolean a_button = gamepad1.a;
+            boolean b_button = gamepad1.b;
+            double seconds = 0;
+            double rate = 2; // inches for 1 rotation
+
+            // determine the number of seconds from the bottom the linear slide has to run for
+            // decimal-ly numbers based on heights of the wedding cake thingy
+            if (y_button) {
+                seconds = 0.4 / rate;
+                startTimer = motorTimer.time();
+                telemetry.addData("Linear Slide", "Going to level 1");
+            } else if (x_button) {
+                seconds = 1.2 / rate;
+                startTimer = motorTimer.time();
+                telemetry.addData("Linear Slide", "Going to level 2");
+            } else if (a_button) {
+                seconds = 2.0 / rate;
+                startTimer = motorTimer.time();
+                telemetry.addData("Linear Slide", "Going to level 3");
+            } else if (b_button) {
+                linearSlide.setPower(-1);
+                telemetry.addData("Linear Slide", "Going down");
+            }
             telemetry.update();
 
+            while (motorTimer.time() < startTimer + seconds) {
+                linearSlide.setPower(1);
+            }
+            linearSlide.setPower(0);
 
-            // Output the safe vales to the motor drives.
-            //robot.leftDrive.setPower(drive); // originally left
-//            robot.rightDrive.setPower(right);
-
-            // Use gamepad left & right Bumpers to open and close the claw
-//            if (gamepad1.right_bumper)
-//                clawOffset += CLAW_SPEED;
-//            else if (gamepad1.left_bumper)
-//                clawOffset -= CLAW_SPEED;
-
-            // Move both servos to new position.  Assume servos are mirror image of each other.
-//            clawOffset = Range.clip(clawOffset, -0.5, 0.5);
-//            robot.leftClaw.setPosition(robot.MID_SERVO + clawOffset);
-//            robot.rightClaw.setPosition(robot.MID_SERVO - clawOffset);
-
-            // Use gamepad buttons to move arm up (Y) and down (A)
-//            if (gamepad1.y)
-//                robot.leftArm.setPower(robot.ARM_UP_POWER);
-//            else if (gamepad1.a)
-//                robot.leftArm.setPower(robot.ARM_DOWN_POWER);
-//            else
-//                robot.leftArm.setPower(0.0);
-
-            // Send telemetry message to signify robot running;
-//            telemetry.addData("claw",  "Offset = %.2f", clawOffset);
-//            telemetry.addData("left",  "%.2f", left);
-//            telemetry.addData("right", "%.2f", right);
-//               telemetry.addData("yay!");
-            //telemetry.update();
-
-            // Pace this loop so jaw action is reasonable speed.
             idle();
-            // }
         }
     }
 }
